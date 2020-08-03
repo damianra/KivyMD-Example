65dfa64ff2a5e4824d22ebcc511b8867e6b39121
damianra <44167699+damianra@users.noreply.github.com>
2020-08-03 10:52:22 -0300

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # KivyMD-Example
+
+This is a practice of android applications with kivy and kivymd. <br>
+This application connects to the APIRest of https://www.coingecko.com/es obtaining the price of Bitcoin at the moment,<br>
+it also allows us to calculate the amount of BTC according to the amount of dollars entered.
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from functools import partial
+
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import requests
+from kivy.network.urlrequest import UrlRequest
 
 from kivymd.toast import toast
 
@@ -12,7 +14,6 @@ class MainWid(ScreenManager):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.Home = Home()
-        Builder.load_file('main.kv')
         wid = Screen(name='home')
         wid.add_widget(self.Home)
         self.add_widget(wid)
@@ -22,19 +23,18 @@ class Home(BoxLayout):
     def __init__(self, **kwargs):
         super(Home, self).__init__()
         self.price = .0
-        self.btc_price()
+        # self.btc_price()
+        self.url_requests()
 
+    def url_requests(self, *args):
+        req = UrlRequest('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false', self.got_json)
 
-    def btc_price(self):
-        r = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
-        data = []
-        if r.status_code == 200:
-            json = r.json()
-            for j in json:
-                data.append(j)
-        self.ids.pricelbl.text = '1 BTC = ' + str(data[0]['current_price']) + ' USD'
-        self.price = data[0]['current_price']
-        print(self.price)
+    def got_json(self, req, result):
+        for key,value in result[0].items():
+            if key == 'current_price':
+                self.ids.pricelbl.text = '1 BTC = ' + str(value) + ' USD'
+                self.price = value
+
 
     def btc_convert(self):
         cant = float(self.ids.btctext.text)
